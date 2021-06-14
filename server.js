f613d400f4cf0feb0f894e028c26436277124124
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-06-14 15:26:30 -0500

    Tested server to make sure it is running. It works with no errors.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+require("dotenv").config(); // Bring in .env variables
 const express = require("express");
 const app = express();
-const PORT = process.env || 3000;
+const PORT = process.env.PORT || 3001;
+
+// app.get("/", (req, res) => {
+// 	res.send("Hello World");
+// });
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}.`);
